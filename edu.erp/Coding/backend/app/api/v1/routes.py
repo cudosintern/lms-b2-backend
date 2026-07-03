388eb3cac2fb635a9e19cc671fc6f20c8300fc12
saniyasullad <saniyasullad28@gmail.com>
2026-07-04 02:03:36 +0530

    student registration module

--- edu.erp/Coding/backend/app/api/v1/routes.py
+++ edu.erp/Coding/backend/app/api/v1/routes.py
@@ -37,6 +37,13 @@ from app.api.v1.cudo_module.lab_category.lab_category_api import (
     router as lab_category_router
 )
 
+from app.api.v1.cudo_module.board_of_studies.api.bos_member_api import (
+    router as bos_member_router
+)
+
+from app.api.v1.cudo_module.delivery_method.api.delivery_method_api import (
+    router as delivery_method_router
+)
 
 from app.api.v1.cudo_module.manage_knowledge_and_attitude_profile.api import (
     router as manage_knowledge_and_attitude_profile_router
@@ -82,6 +89,9 @@ from app.api.v1.lms_module.cross_department_mentor import router as cross_depart
 from app.api.v1.lms_module.mentor_list import router as mentor_list_router
 from app.api.v1.lms_module.mentoring import router as mentoring_router
 from app.api.v1.lms_module.student_details import router as student_details_router
+from app.api.v1.lms_module.lms_student_course_registration.lms_student_course_registration import (
+    router as lms_student_course_registration
+)
 
 router = APIRouter()
 
@@ -102,6 +112,10 @@ router.include_router(login.router, prefix="/staff_student_login", tags=["Login"
 
 router.include_router(
     program_mode_router, prefix="/program_mode", tags=["Program Mode"]
+)
+
+router.include_router(
+    lms_student_course_registration, prefix="/lms_student_course_registration", tags=["LMS-registration"]
 )
 
 router.include_router(
